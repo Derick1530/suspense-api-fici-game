9e3156955f39f9e01be7419340520b894e1760de
Derick1530 <derickzihalirwa07@gmail.com>
2022-08-27 18:41:20 +0300

    codebase

--- web-client/src/App.jsx
+++ web-client/src/App.jsx
@@ -1,11 +1,20 @@
 import React, { lazy, Suspense } from 'react'
-import FiciGame from './page/FiciGame';
+import { Route, Routes } from 'react-router-dom'
+import FiciGame from './page'
+import SinglePost from './page/singlePost'
 
 const App = () => {
     return (
         // Show a spinner while the profile is loading
-        <div className='mx-auto w-4/12 p-8 flex justify-center self-center shadow-xl rounded-md bg-gray-900'>
-            <FiciGame />
+        <div className='mx-auto w-full md:w-2/5 p-4 shadow-xl rounded-md bg-gray-900'>
+            <Routes>
+                <Route path='/' element={
+                    <FiciGame />
+                } />
+                <Route path='/:id' element={
+                    <SinglePost />
+                } />
+            </Routes>
         </div>
     )
 }
